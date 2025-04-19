319e3947f5da85269d50ab9382334f289af80fec
DavronRaimjonov <davron_raimjonov4446@mail.ru>
2025-04-19 18:20:28 +0500

    fix: edited cookie

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -17,8 +17,8 @@ export const sendTokenAsCookie = (res, token) => {
   res.cookie("token", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV === "production" ? true : false,
-    sameSite: "lax",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
     maxAge,
   });
 };
-``
+``;
